08c22b3f264f1c221e1f90c76ae24ee248a316a9
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2025-09-25 11:53:52 +0200

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  base: '/cleoqinportfolio1758794028963/',
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          three: ['three', '@react-three/fiber', '@react-three/drei']
+        }
+      }
+    }
+  }
+})
